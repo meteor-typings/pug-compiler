18a0d802e888caf26f904661a458637a4aec5a9c
Artur Nerkowski <artur@reaktor5.com>
2017-03-20 05:32:25 +0100

    Added description

--- main.d.ts
+++ main.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for apatryda:pug-compiler v0.0.2
+// Project: https://github.com/apatryda/pug-compiler
+// Definitions by: Artur Nerkowski <https://github.com/apatryda>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module "*.pug" {
   const template: string;
   export default template;
