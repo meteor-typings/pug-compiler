100aefe8b4d43017020b90a6f8760dfc94c755b4
Artur Nerkowski <artur@reaktor5.com>
2017-03-20 05:21:01 +0100

    Prepared first typings

---
+++ meteor-pug-compiler.tests.ts
@@ -0,0 +1,7 @@
+import test1 from './tests/test1.pug';
+import test2 from './tests/test2.pug';
+import test3 from './tests/test3.pug';
+
+console.log('test1', test1);
+console.log('test2', test2);
+console.log('test3', test3);
